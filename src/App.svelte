90ec9a32f3cebcd5340517e010a0b74dd1552aff
Jaya Sharma <sharmajayanilesh@gmail.com>
2021-11-05 19:13:20 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Space from './Space.svelte';
+</script>
+
+<style>
+	main {
+		width: 475px;
+		margin: 0 auto;
+		height: 1000px;
+	}
+
+	button {
+		width: 100%;
+		margin-top: 20px;
+		border-radius: 0;
+		background-color: lightblue;
+		font-size: 30px;
+		cursor: pointer;
+	}
+
+	button:hover {
+		outline: none;
+	}
+
+	.row {
+		display: flex;
+	}
+</style>
+
+<main>
+	<h1>Tic Tac Toe</h1>
+	<h2>Number of players:</h2>
+	<h2>Player X</h2>
+	<div class="row">
+		<Space space="X" />
+		<Space space="X" />
+		<Space space="X" />
+	</div>
+	<div class="row">
+		<Space space="X" />
+		<Space space="X" />
+		<Space space="X" />
+	</div>
+	<div class="row">
+		<Space space="X" />
+		<Space space="X" />
+		<Space space="X" />
+	</div>
+	<button>New Game</button>
+</main>
+
---
+++ src/Space.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let space = "";
+</script>
+
+<style>
+    .space {
+        border: solid 1px gray;
+        height: 125px;
+        width: 125px;
+        flex: 1;
+        align-content: center;
+        justify-content: center;
+        font-size: 75px;
+        text-align: center;
+        line-height: 125px;
+        cursor: pointer;
+    }
+</style>
+
+<div class="space">
+    {space}
+</div>
